5f20d8b777bf298a11de5a57f8f3df81115f0883
Harshith0027 <132474027+Harshith0027@users.noreply.github.com>
2025-01-13 18:09:38 +0530

    Update main.jsx

--- Task-Management/src/main.jsx
+++ Task-Management/src/main.jsx
@@ -5,7 +5,9 @@ import { Provider } from 'react-redux'
 import { store } from './app/store.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
