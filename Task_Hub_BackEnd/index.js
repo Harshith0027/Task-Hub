3b801c518656e37269ebae6bcad998327dcd21e2
Harshith0027 <132474027+Harshith0027@users.noreply.github.com>
2025-01-13 21:24:32 +0530

    Update index.js

--- Task_Hub_BackEnd/index.js
+++ Task_Hub_BackEnd/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import userRouter from "./routes/UserRoute.js";
 import taskRouter from "./routes/TaskRoute.js";
+import path from 'path';
 
 const dbConnect = async () => {
     await connectToMongo();
@@ -14,6 +15,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth",userRouter);
 app.use("/api/tasks",taskRouter);
+app.get('/vite.svg', (req, res) => {
+    res.type('image/svg+xml');
+    res.sendFile(path.join(__dirname, '../Task-Management/public/vite.svg'));
+});
+// Serve other static files
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static('dist', {
   setHeaders: (res, path) => {
     if (path.endsWith('.js')) {
